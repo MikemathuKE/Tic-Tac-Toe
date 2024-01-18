8781c698916ee24e4b0af2ab97f26acd72f4d0e6
MikemathuKE <michaelmathu6@gmail.com>
2024-01-18 17:31:21 +0300

    game logic

--- main.py
+++ main.py
@@ -19,48 +19,56 @@ class TicTacToe:
         self.turn = "X"
         self.main_font = font.Font(family="Helvetica", size=10, weight="bold")
 
-        self.a1_coord = [0, 0]
-        self.a1 = tk.Button(self.main_frame, textvariable=self.game_grid[0][0], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(self.a1_coord))
-        self.a1.grid(row=0, column=0, padx=5, pady=5, sticky="nw")
+        a1_coord = [0, 0]
+        a1 = tk.Button(self.main_frame, textvariable=self.game_grid[0][0], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(a1_coord))
+        a1.grid(row=0, column=0, padx=5, pady=5, sticky="nw")
 
-        self.a2_coord = [0, 1]
-        self.a2 = tk.Button(self.main_frame, textvariable=self.game_grid[0][1], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(self.a2_coord))
-        self.a2.grid(row=0, column=1, padx=5, pady=5, sticky="n")
+        a2_coord = [0, 1]
+        a2 = tk.Button(self.main_frame, textvariable=self.game_grid[0][1], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(a2_coord))
+        a2.grid(row=0, column=1, padx=5, pady=5, sticky="n")
 
-        self.a3_coord = [0, 2]
-        self.a3 = tk.Button(self.main_frame, textvariable=self.game_grid[0][2], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(self.a3_coord))
-        self.a3.grid(row=0, column=2, padx=5, pady=5, sticky="ne")
+        a3_coord = [0, 2]
+        a3 = tk.Button(self.main_frame, textvariable=self.game_grid[0][2], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(a3_coord))
+        a3.grid(row=0, column=2, padx=5, pady=5, sticky="ne")
 
-        self.b1_coord = [1, 0]
-        self.b1 = tk.Button(self.main_frame, textvariable=self.game_grid[1][0], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(self.b1_coord))
-        self.b1.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+        b1_coord = [1, 0]
+        b1 = tk.Button(self.main_frame, textvariable=self.game_grid[1][0], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(b1_coord))
+        b1.grid(row=1, column=0, padx=5, pady=5, sticky="w")
 
-        self.b2_coord = [1, 1]
-        self.b2 = tk.Button(self.main_frame, textvariable=self.game_grid[1][1], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(self.b2_coord))
-        self.b2.grid(row=1, column=1, padx=5, pady=5)
+        b2_coord = [1, 1]
+        b2 = tk.Button(self.main_frame, textvariable=self.game_grid[1][1], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(b2_coord))
+        b2.grid(row=1, column=1, padx=5, pady=5)
 
-        self.b3_coord = [1, 2]
-        self.b3 = tk.Button(self.main_frame, textvariable=self.game_grid[1][2], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(self.b3_coord))
-        self.b3.grid(row=1, column=2, padx=5, pady=5, sticky="e")
+        b3_coord = [1, 2]
+        b3 = tk.Button(self.main_frame, textvariable=self.game_grid[1][2], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(b3_coord))
+        b3.grid(row=1, column=2, padx=5, pady=5, sticky="e")
 
-        self.c1_coord = [2, 0]
-        self.c1 = tk.Button(self.main_frame, textvariable=self.game_grid[2][0], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(self.c1_coord))
-        self.c1.grid(row=2, column=0, padx=5, pady=5, sticky="sw")
+        c1_coord = [2, 0]
+        c1 = tk.Button(self.main_frame, textvariable=self.game_grid[2][0], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(c1_coord))
+        c1.grid(row=2, column=0, padx=5, pady=5, sticky="sw")
 
-        self.c2_coord = [2, 1]
-        self.c2 = tk.Button(self.main_frame, textvariable=self.game_grid[2][1], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(self.c2_coord))
-        self.c2.grid(row=2, column=1, padx=5, pady=5, sticky="s")
+        c2_coord = [2, 1]
+        c2 = tk.Button(self.main_frame, textvariable=self.game_grid[2][1], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(c2_coord))
+        c2.grid(row=2, column=1, padx=5, pady=5, sticky="s")
 
-        self.c3_coord = [2, 2]
-        self.c3 = tk.Button(self.main_frame, textvariable=self.game_grid[2][2], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(self.c3_coord))
-        self.c3.grid(row=2, column=2, padx=5, pady=5, sticky="se")
+        c3_coord = [2, 2]
+        c3 = tk.Button(self.main_frame, textvariable=self.game_grid[2][2], bg="white", fg="black", width=20, height=10, font=self.main_font, command= lambda: self.button_press(c3_coord))
+        c3.grid(row=2, column=2, padx=5, pady=5, sticky="se")
+
+        self.buttons = [
+            [a1, a2, a3],
+            [b1, b2, b3],
+            [c1, c2, c3]
+        ]
 
         self.main_frame.pack()
 
         self.win_text = tk.StringVar()
-        self.win_frame = tk.Frame(window, bg="black", width=400, height=400, padx=65, pady=65)
-        self.win_label = tk.Label(self.win_frame, textvariable=self.win_text, bg="green", fg="white", width=400, height=400, font=self.main_font)
+        self.win_frame = tk.Frame(window, bg="black", width=400, height=400, padx=30, pady=30)
+        self.win_label = tk.Label(self.win_frame, textvariable=self.win_text, bg="green", width=400, height=31, fg="white", font=self.main_font)
+        self.win_button = tk.Button(self.win_frame, text="Play Again", bg="white", fg="black", width=350, height=2, font=self.main_font, command= lambda: self.restart_game())
         self.win_label.pack()
+        self.win_button.pack()
 
     def change_turn(self):
         if self.turn == "X":
@@ -70,12 +78,15 @@ class TicTacToe:
 
     def check_win(self):
         horizontal = ""
+        filled = 0
         for row in self.game_grid:
-            horizontal = (val.get() for val in row)
+            horizontal = ''.join([val.get() for val in row])
             if horizontal == "XXX":
                 return "X"
             elif horizontal == "OOO":
                 return "O"
+            
+            filled += len(str(horizontal))
             horizontal = ""
             
         vertical = ""
@@ -104,15 +115,14 @@ class TicTacToe:
         elif diagonal == "OOO":
             return "O"
         
-        print(len(vertical), len(horizontal), len(diagonal))
-        
-        if len(vertical) == 3 and len(horizontal) == 3 and len(diagonal) == 3:
+        if filled == 9:
             return "X0"
         
         return None
     
     def button_press(self, coord):
         self.game_grid[coord[0]][coord[1]].set(self.turn)
+        self.buttons[coord[0]][coord[1]]['state'] = tk.DISABLED
         self.change_turn()
         result = self.check_win()
         if result:
@@ -122,6 +132,18 @@ class TicTacToe:
                 self.win_text.set(result + " wins!")
             self.main_frame.pack_forget()
             self.win_frame.pack()
+
+    def restart_game(self):
+        self.main_frame.pack()
+        self.win_frame.pack_forget()
+        self.win_text.set("")
+        self.turn = "X"
+        for row in self.game_grid:
+            for val in row:
+                val.set("")
+        for row in self.buttons:
+            for val in row:
+                val['state'] = tk.NORMAL
 
         
 
